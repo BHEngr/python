ebb20161075b369a993e7a0a2f459236805ac777
corbin <cjmcherron@mavs.coloradomesa.edu>
2024-05-21 20:17:23 -0600

    Hello World print sum

---
+++ HelloWorld/main.py
@@ -0,0 +1,9 @@
+
+def Hello():
+        print("Hello World!")
+        print("new line, within Hello() function")
+        
+        
+Hello()
+
+print("Hello, outside of the function")
---
+++ Summation/main.py
@@ -0,0 +1,12 @@
+
+def summing(valOne, valTwo): 
+        val1 = int(valOne)
+        val2 = int(valTwo)
+        x = val1 + val2
+        print("Value:",x)    
+      
+#Basic Summation formula        
+
+valOne = input("Enter Value One: ")
+valTwo = input("Enter Value Two: ")
+summing(valOne, valTwo)
